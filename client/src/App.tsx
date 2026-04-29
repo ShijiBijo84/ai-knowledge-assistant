5abd1e8c841333d17fd90ce18b432157eadf6be6
shijibijo84 <shijib.bijo@gmail.com>
2026-04-29 12:21:50 +0200

    first chat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,121 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useRef, useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [messages, setMessages] = useState([])
+    const [input, setInput] = useState("")
+    const [loading, setLoading] = useState(false)
+    const bottomRef = useRef(null);
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    useEffect(() => {
+        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
 
-      <div className="ticks"></div>
+    const sendMessage = async () => {
+        if (!input.trim()) return
+        const userMessage = { role: 'user', content: input }
+        setMessages(prev => [...prev, userMessage])
+        setInput("")
+        setLoading(true)
+        try {
+            const res = await fetch("http://localhost:3000/api/chat", {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ history: messages, userMessage })
+            })
+            const data = await res.json()
+            const { response, reasoning_details } = data
+            setMessages((prev) => [...prev, { role: "assistant", content: response, reasoning_details }])
+            setLoading(false)
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    return (
+        <div className="flex h-screen text-white">
+            <div className="w-64 bg-gray-800 ">Sidebar</div>
+            <div className="flex-1 bg-gray-900 flex flex-col">
+                <div className="p-4 border-b border-gray-700">Your AI Assistant</div>
+                <div className="flex-1 p-4 overflow-y-auto space-y-4">
+                    {messages.map((msg, index) => (
+                        <div key={index}
+                            className={`flex ${msg.role === 'user' ? "justify-end" : "justify-start"}`}
+                        >
+                            <div className={`px-4 py-2 rounded-xl max-w-xs
+                                ${msg.role === "user" ? "bg-blue-600" : "bg-gray-700"}`
+                            }>
+                                {msg.content}
+                            </div>
+                        </div>
+                    ))}
+                    {loading && <div className="justify-start w-20 flex bg-gray-700 px-4 py-2 rounded-xl space-x-2">
+                        <span className="animate-bounce">.</span>
+                        <span className="animate-bounce delay-150">.</span>
+                        <span className="animate-bounce delay-300">.</span>
+                    </div>}
+                    <div ref={bottomRef} />
+                </div>
+                <div className="p-4 border-t border-gray-700">
+                    <div className="flex items-center bg-gray-800 rounded-xl px-4 py-2 ">
+                        <input
+                            value={input}
+                            disabled={loading}
+                            onChange={(e) => setInput(e.target.value)}
+                            className="flex-1 bg-transparent outline-none text-white"
+                            placeholder="Type a message..."
+                            onKeyDown={(e) => e.key === "Enter" && sendMessage()} />
+                        <button
+                            onClick={sendMessage}
+                            className=" bg-blue-600 px-4 py-1 rounded-lg">Send</button>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
 }
-
 export default App
