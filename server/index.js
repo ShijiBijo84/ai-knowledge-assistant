3496f38ece3330cb76c0f10b8b92150497deb3cc
shijibijo84 <shijib.bijo@gmail.com>
2026-06-18 23:40:27 +0200

    rag-prompt  and ui improved

--- server/index.js
+++ server/index.js
@@ -17,26 +17,58 @@ app.get("/{*any}", (req, res) => {
     res.sendFile(path.join(__dirName, "../client/dist/index.html"))
 })
 
+//---------------------------------
+// Vector Store initialization
+//---------------------------------
 
-let vectorStore;
-(async () => { vectorStore = await createVectorStore() })()
+let vectorStore = null;
+console.log("Vector Store Loading...")
+vectorStore = await createVectorStore()
+console.log("Vector Store Loaded")
 
 
 app.post('/api/chat', async (req, res) => {
     try {
-        const { history, userMessage } = req.body
+        const { history = [], userMessage } = req.body
 
-        if (!userMessage) return res.status(400).json({ error: "Message required" });
+        if (!userMessage?.content?.trim()) return res.status(400).json({ error: "Message required" });
 
         const docs = await search(vectorStore, userMessage.content)
+
         const context = docs.map(d => d.text).join("\n\n")
+
         const messages = [...(history || []), userMessage]
+
+        const systemPrompt = `
+You are a recipe assistant.
+
+Always respond in VALID MARKDOWN.
+
+FORMAT:
+
+**Recipe:** <name>
+
+**Cooking Time:** <time>
+
+**Ingredients**
+Use a Markdown list.
+
+**Instructions**
+Use a numbered list.
+
+Do NOT use any special symbols like "-" manually.
+Let Markdown format lists naturally.
+
+Knowledge Base:
+${context}
+`;
         const apiResponse = await client.chat.completions.create(
             {
                 model: "openai/gpt-oss-120b",
+                temperature: 0,
                 messages: [{
                     role: "system",
-                    content: `Answer using this context:\n\n${context}`,
+                    content: systemPrompt,
                 }, ...messages]
             }
         )
